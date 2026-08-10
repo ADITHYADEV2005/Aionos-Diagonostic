485e3398eb23ed8fa58ec896050d7ad5a49ecf8f
ADITHYADEV2005 <adithyadevkichu@gmail.com>
2026-08-10 21:33:14 +0530

    fix(backend): un-ignore backend/models/ directory and add User.js model

---
+++ backend/models/User.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+import bcryptjs from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide a name"],
+    trim: true,
+    maxlength: [50, "Name cannot be more than 50 characters"]
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+    unique: true,
+    lowercase: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please provide a valid email"
+    ]
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minlength: 6,
+    select: false // Don't return password by default
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Hash password before saving
+userSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  
+  const salt = await bcryptjs.genSalt(10);
+  this.password = await bcryptjs.hash(this.password, salt);
+});
+
+// Method to compare passwords
+userSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcryptjs.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
